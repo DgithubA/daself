12833885e280dd69a5fbd1eaad18ae1d467a7b28
DANA <bibaks34@gmail.com>
2024-07-29 23:24:23 +0330

    [skip ci]

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 $settings = new Settings();
-$settings->setAppInfo((new Settings\AppInfo())->setApiId($_ENV['API_ID'])->setApiHash($_ENV['API_HASH'])->setAppVersion($_ENV['VERSION'])->setDeviceModel($_ENV['DEVICE_MODEL']));
+$settings->setAppInfo((new Settings\AppInfo())->setApiId((int)$_ENV['API_ID'])->setApiHash($_ENV['API_HASH'])->setAppVersion($_ENV['VERSION'])->setDeviceModel($_ENV['DEVICE_MODEL']));
 
 !is_dir('./sessions') && mkdir('./sessions');
 APP\botHandler::startAndLoop('./sessions/daself.madeline',$settings);
