debde90a67b6d28828d15bd78ef714c251bab9d1
DANA <bibaks34@gmail.com>
2024-08-23 18:22:32 +0330

    redis database & auto login in

--- bot.php
+++ bot.php
@@ -2,13 +2,18 @@
 
 require_once 'vendor/autoload.php';
 
+use danog\MadelineProto\API;
 use danog\MadelineProto\Settings;
+
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 $settings = new Settings();
 $settings->setAppInfo((new Settings\AppInfo())->setApiId((int)$_ENV['API_ID'])->setApiHash($_ENV['API_HASH'])->setAppVersion($_ENV['VERSION'])->setDeviceModel($_ENV['DEVICE_MODEL']));
-$settings->setDb((new Settings\Database\Postgres())->setUri('tcp://postgres')->setDatabase('postgres')->setPassword('postgres')->setUsername('postgres'));
+//$settings->setDb((new Settings\Database\Postgres())->setUri('tcp://postgres')->setDatabase('postgres')->setPassword('postgres')->setUsername('postgres'));
+$settings->setDb((new Settings\Database\Redis())->setUri('redis://redis'));
+
 
 $session_dir = $_ENV['SESSION_DIR'];
 str_ends_with($session_dir , '/') && $session_dir = substr($session_dir, 0, -1);
@@ -20,19 +25,22 @@ $session_dir = './'.$session_dir;
 
 $session_file = $session_dir . DIRECTORY_SEPARATOR . 'daself.madeline';
 
-if(isset($argv[1]) and $argv[1] == '--just-login') {
-    try {
-        $api = new \danog\MadelineProto\API($session_file,$settings);
-        $get_self = $api->start();
-        if(!$api->isSelfUser()){
-            echo "Pleace login as user.";
-            $api->logout();
-            rmdir($session_dir);
-        }else echo "Successfully logged in as user.";
-    }catch (\Exception $e){
-        echo $e;
-    }
-    exit(0);
+try {
+    $api = new API($session_file,$settings);
+}catch (\Exception $e){
+    echo $e;
+    exit(1);
 }
 
+if($api->getAuthorization() !==  API::LOGGED_IN || (isset($argv[1]) and $argv[1] == '--just-login')) {
+    $get_self = $api->start();
+}
+
+if(!$api->isSelfUser()){
+    echo "Pleace login as user.";
+    $api->logout();
+    rmdir($session_dir);
+    exit(1);
+}else echo "Successfully login in as user.";
+unset($api);
 APP\botHandler::startAndLoop($session_file,$settings);
