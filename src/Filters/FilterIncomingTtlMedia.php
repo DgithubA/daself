2129db318586674db7941a17ae73c6012686180c
Piagrammist <Piagrammist@gmail.com>
2024-12-20 19:59:19 +0330

    Cleanup & improve logic of all Filters

--- src/Filters/FilterIncomingTtlMedia.php
+++ src/Filters/FilterIncomingTtlMedia.php
@@ -3,43 +3,24 @@
 namespace App\Filters;
 
 use Attribute;
-use danog\MadelineProto\EventHandler;
-use danog\MadelineProto\EventHandler\AbstractMessage;
-use danog\MadelineProto\EventHandler\Filter\Combinator\FiltersAnd;
-use danog\MadelineProto\EventHandler\Filter\Filter;
-use danog\MadelineProto\EventHandler\Filter\FilterIncoming;
-use danog\MadelineProto\EventHandler\Filter\FilterMedia;
-use danog\MadelineProto\EventHandler\Filter\FilterPrivate;
+
+use danog\MadelineProto\EventHandler\Update;
 use danog\MadelineProto\EventHandler\Media\Photo;
 use danog\MadelineProto\EventHandler\Media\Video;
-use danog\MadelineProto\EventHandler\Message;
+use danog\MadelineProto\EventHandler\Filter\Filter;
 use danog\MadelineProto\EventHandler\Message\PrivateMessage;
-use danog\MadelineProto\EventHandler\Update;
-use danog\MadelineProto\VoIP;
-use danog\MadelineProto\VoIP\CallState;
 
-/**
- * Use with #[FilterIncomingTtlMedia]
- */
 #[Attribute(Attribute::TARGET_METHOD)]
 final class FilterIncomingTtlMedia extends Filter
 {
-
-    public function initialize(EventHandler $API): Filter
-    {
-        return $this;
-        //return (new FiltersAnd(new FilterMedia(), new FilterIncoming(),new FilterPrivate()))->initialize($API);
-    }
-
     public function apply(Update $update): bool
     {
-        $FilterPrivate = $update instanceof PrivateMessage;
-
-        $FilterMedia = $update instanceof Message && $update->media !== null;
-
-        $FilterIncoming = ($update instanceof AbstractMessage && !$update->out)
-            || ($update instanceof VoIP && $update->getCallState() === CallState::INCOMING);
-
-        return $FilterPrivate and $FilterMedia and $FilterIncoming and (($update->media instanceof Photo || $update->media instanceof Video) and $update->media->ttl !== null);
+        return (
+            $update instanceof PrivateMessage &&
+            !$update->out &&
+            $update->media !== null &&
+            ($update->media instanceof Photo || $update->media instanceof Video) &&
+            $update->media->ttl !== null
+        );
     }
 }
--- src/Filters/FilterUserStatus.php
+++ src/Filters/FilterUserStatus.php
@@ -8,13 +8,9 @@ use danog\MadelineProto\EventHandler\Filter\Filter;
 use danog\MadelineProto\EventHandler\User\Status\Offline;
 use danog\MadelineProto\EventHandler\User\Status\Online;
 
-/**
- * Use with #[FilterUserStatus]
- */
 #[Attribute(Attribute::TARGET_METHOD)]
 final class FilterUserStatus extends Filter
 {
-
     public function apply(Update $update): bool
     {
         return $update instanceof Offline || $update instanceof Online;
